f8bd00a42ee9df76f015afefe154ac67b1fe779b
cpysleeper <54315871+cpysleeper@users.noreply.github.com>
2019-08-27 22:41:48 -0400

    Update A1Jedi.java

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -10,5 +10,113 @@ public class A1Jedi {
 
 		// Your code follows here.
 		
+
+				
+				int numitems = scan.nextInt();
+				String [] namefruit = new String[numitems];
+				double [] prices = new double[numitems];
+				int [] countfruit = new int[numitems];
+				for(int l=0; l < numitems ; l ++)
+				{
+					countfruit[l] = 0;
+				}
+			    
+				int [] countcus = new int[numitems];
+				for(int l=0; l < numitems ; l ++)
+				{
+					countcus[l] = 0;
+				}
+				
+				for(int m=0; m < numitems; m++)
+				{
+					namefruit[m]= scan.next();
+					prices[m] = scan.nextDouble();
+				}
+				int customeramount = scan.nextInt();
+				double [] totalprice = new double[customeramount];
+				
+				String [] names = new String[customeramount];
+				
+				for(int i=0; i < customeramount; i++)
+				{  
+					String first = scan.next();
+					String last = scan.next();
+					
+					names[i]  = first + " " + last;
+					int amountfruit = scan.nextInt();
+					double priceofone = 0; 
+					
+					for(int j = 0; j< amountfruit; j++) {
+					int num1 = scan.nextInt();
+					String fruit = scan.next();
+					int fruitindex = 0;
+					
+						for(int n = 0; n < numitems ;n++)
+						{
+							if (fruit.equals(namefruit[n]))
+							fruitindex = n;
+						}
+						
+						
+						
+						countfruit[fruitindex] = num1 + countfruit[fruitindex];
+					    countcus[fruitindex] = countcus[fruitindex] + 1;
+						
+					double price1 = prices[fruitindex];
+					priceofone = price1 * num1 + priceofone;
+					}
+		            totalprice[i] = priceofone;
+							
+					}
+				int min = 0; 
+				double comparep = 111111111;
+				for(int h = (customeramount - 1); h >= 0  ; h --)
+			    {
+			    
+			    if (comparep > totalprice[h])
+			        min= h;
+			        comparep = totalprice[h];
+			    	
+			    }
+				  
+					
+					
+				int max =  0;
+				double comparep2 = 0.0;
+				for(int s = 0; s < customeramount ; s ++)
+				    {
+				    
+				    if (totalprice[s] < comparep2)
+				        max= s;
+				        comparep2 = totalprice[s];
+				    }
+				
+				double totalforall = 0;
+				
+				for(int o = 0; o < totalprice.length; o++)
+				{
+					totalforall = totalprice[o] + totalforall;
+				}
+				
+				
+				
+				
+				
+			for(int x = 0; x < numitems; x++ )
+			{
+				if (countfruit[x] == 0)
+					System.out.println("No customers bought " + namefruit[x]);
+				else
+					System.out.println(countcus[x] + " customers bought " + countfruit[x] + " " + namefruit[x]);
+			}
+				
+	
+	
 	}
+			
+
+
+
+
+	
 }
